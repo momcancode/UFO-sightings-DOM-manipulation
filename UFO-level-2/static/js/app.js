4e6e0f21a4953ee2db8e581e3241b07402e22982
Tracey Phuong Ha <thao.ph.ha@gmail.com>
2020-08-29 16:25:26 +1000

    Select input values of 5 filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,7 +1,7 @@
 // from data.js
 var tableData = data;
 
-// Level 2: Multiple Search Categories
+// LEVEL 2: MULTIPLE SEARCH CATEGORIES
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
@@ -60,29 +60,46 @@ function runEnter() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select(".form-control");
+	// Select the input elements and get the values of the input elements
+	var countryInputValue = d3.select("#country").property("value");
+	var stateInputValue = d3.select("#state").property("value");
+	var shapeInputValue = d3.select("#shape").property("value");
+	var cityInputValue = d3.select("#city").property("value");
+	var dateInputValue = d3.select("#datetime").property("value");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-
-  // Use the form input to filter the data by datetime
-	var results = tableData.filter(ufo => ufo.datetime === inputValue);
-	
-	// Clear out current contents in the table
-	tbody.html("");
-
-	// Handle no matching results
-	if (results.length === 0) {
-		tbody.text(`No ufo sightings on ${inputValue}.`);
+	// Create filter object
+	var filter = {
+		country: countryInputValue,
+		state: stateInputValue,
+		shape: shapeInputValue,
+		city: cityInputValue,
+		date: dateInputValue
 	}
-	else {
-		results.forEach((ufo) => {
-			var row = tbody.append("tr");
-			Object.entries(ufo).forEach(([key, value]) => {
-				var cell = row.append("td");
-				cell.text(value);
+		var inputValue = inputElements[i].property("value");
+
+
+		// Use the form input to filter the data by datetime
+		var result_country = tableData.filter(ufo => ufo.country === inputValue);
+		
+		// Clear out current contents in the table
+		tbody.html("");
+
+		// Handle no matching results
+		if (results.length === 0) {
+			tbody.text(`No ufo sightings on ${inputValue}.`);
+		}
+		else {
+			results.forEach((ufo) => {
+				var row = tbody.append("tr");
+				Object.entries(ufo).forEach(([key, value]) => {
+					var cell = row.append("td");
+					cell.text(value);
+				});
 			});
-		});
-	};
+		};
+		}
+
+  
+
+
 };
